8a60281651407fa65d3e3599b0ec18704bb6ef26
sahana-gudi <sonugudi472@gmail.com>
2025-12-26 23:19:33 +0530

    Add unit test for employee_details function

---
+++ test_employee.py
@@ -0,0 +1,13 @@
+# test_employee.py
+
+from employee import employee_details
+
+def test_employee_details():
+    expected_output = (
+        "Employee Name: Alice\n"
+        "Employee ID: E1001\n"
+        "Department: IT\n"
+        "Salary: 55000"
+    )
+
+    assert employee_details("Alice", "E1001", "IT", 55000) == expected_output
